2c14fac87337e39aa7e5ebf139ac87cadd9d5714
Vicky-Jha <53377467+Vicky-Jha@users.noreply.github.com>
2020-10-23 14:47:25 +0530

    Add files via upload

---
+++ FullDataStructures/priority_queueArray.cpp
@@ -0,0 +1,135 @@
+//Array Queue
+# include <bits/stdc++.h>
+# define max 20
+using namespace std;
+
+int front = 0, rare = 0,ele,pq[max], i,j,temp,del;
+int loc = 0,flag=0;
+void Enqueue(void);
+void Dequeue(void);
+void Display(void);
+int Full(void);
+int Empty(void);
+void findMin();
+
+int main()
+{
+    int ch;
+    while(1)
+    {
+		cout<<"Choose  Option \n";
+		cout<<"1. Insert\n2. Delete\n3. Find Min\n4. Display\n5. Exit\n";
+		
+        scanf("%d",&ch);
+        switch (ch) {
+            case 1:Enqueue();
+                break;
+            case 2:Dequeue();
+                break;
+            case 3:findMin();
+                break;
+            case 4:Display();
+                break;
+            case 5:exit(0);
+                break;
+            default:printf("Invalid option\n");
+                break;
+        }
+    }
+    return 0;
+}
+int Full()
+{
+    if(rare == max)
+        return 1;
+    else
+        return 0;
+}
+int Empty()
+{
+    if(rare == front)
+        return 1;
+    else
+        return 0;
+}
+void Enqueue() // Complexity O(n)
+{
+    if(Full())
+        printf("Queue is Full\n");
+    else
+    {
+        printf("Enter Queue data\n");
+        scanf("%d",&ele);
+        
+        if( front == rare )
+        {
+			pq[front] = ele;rare ++;
+			cout<<"Data "<<ele<<" Inserted successfully\n";
+		}
+		else if( front == rare - 1 )
+		{
+			if ( pq[front] > ele )
+			{
+			pq[rare] = pq[front];
+			pq[front] = ele;
+			}
+			else
+			pq[rare] = ele;
+			
+			rare++;
+			cout<<"Data "<<ele<<" Inserted successfully\n";
+		}
+		else if( ele >= pq[rare-1] )
+		{
+			pq[rare] = ele;
+			rare ++;
+			cout<<"Data "<<ele<<" Inserted successfully\n";
+		}
+		else
+		{
+        for ( i = front; i < rare; ++ i )
+        {
+			if ( ele <= pq[i] )
+			{
+				for ( j = rare - 1; j >= i; -- j )
+					pq[j+1] = pq[j];
+				pq[i] = ele;
+				rare ++;
+				cout<<"Data "<<ele<<" Inserted successfully\n";
+				break;
+			}
+		}
+		}
+	}
+    
+}
+void Dequeue() // complexity O(1)
+{
+    if(Empty())
+        printf("queue is Empty\n");
+    else
+    {
+        front ++;
+    }
+}
+void Display()
+{
+    if(Empty())
+        printf("Queue is Empty\n");
+    else
+    {
+        for (i = front ; i<rare ; i++)
+        {
+            printf("%d ",pq[i]);
+        }
+    }
+    cout<<"\n";
+}
+void findMin() //complexity O(1)
+{
+	if(!Empty())
+	cout<<"Min is "<<pq[front]<<"\n";
+	else
+	cout<<"Queue is empty\n";
+}
+
